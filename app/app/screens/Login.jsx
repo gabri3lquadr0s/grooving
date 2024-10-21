b4c9533d9c854a4d9f668938f1d32467fca14dc3
gabri3lquadr0s <gabrielteodoro0710@gmail.com>
2024-10-21 12:26:18 -0300

    Login base

--- app/app/screens/Login.jsx
+++ app/app/screens/Login.jsx
@@ -1,1 +1,25 @@
-import React from 'react';
+import React, {useState, useRef, useEffect} from "react";
+import {View, StyleSheet, Text, Image, Pressable, TextInput} from "react-native";
+
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    return(
+        <View>
+            <View>
+                {/*<Image style={styles.image} source={require('./assets/logo.png')} />*/}
+                <Text>Grooving</Text>
+            </View>
+            <View>
+                <TextInput style={styles.input} value={email} placeholder={"Email"} onChangeText={(text) => setEmail(text)}></TextInput>
+                <TextInput style={styles.input} value={email} placeholder={"Password"} onChangeText={(text) => setPassword(text)}></TextInput>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Login;
